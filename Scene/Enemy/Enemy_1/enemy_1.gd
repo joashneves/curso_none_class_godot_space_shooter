59673ac727639f737d04ca67dc6a51ea0861c186
joashneves <joashneves@gmail.com>
2026-03-12 18:09:39 -0300

    coloca drop do inimigo

--- Scene/Enemy/Enemy_1/enemy_1.gd
+++ Scene/Enemy/Enemy_1/enemy_1.gd
@@ -4,6 +4,7 @@ extends Area2D
 @export var power_up_chance : float = 8
 const EXPLOSION_VFX = preload("uid://b1gn1m7t7akxn")
 const POWER_UP_SPEED = preload("uid://c5a0c6ofhose4")
+const POWER_UP_SHOOT = preload("uid://bay2yn1oit5ch")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -33,6 +34,8 @@ func Destroy_VFX():
 	get_parent().add_child(expolision_vfx)
 
 func Create_PowerUp():
-	var power_up = POWER_UP_SPEED.instantiate()
+	var escolha = [POWER_UP_SHOOT, POWER_UP_SPEED].pick_random()
+	
+	var power_up = escolha.instantiate()
 	power_up.global_position = global_position
 	get_parent().add_child(power_up)
